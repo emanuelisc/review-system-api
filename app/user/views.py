20d25de788dc3d59b40adbb5f5d2a7895b3f5c65
Emanuelisc <emanuelisc@gmail.com>
2019-03-14 15:27:45 +0200

    Fixed linting problems

--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,9 @@ from rest_framework import generics, authentication, permissions, viewsets
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer, AuthTokenSerializer, AdminUsersSerializer
+from user.serializers import UserSerializer, \
+                            AuthTokenSerializer, \
+                            AdminUsersSerializer
 
 from core.models import User
 
